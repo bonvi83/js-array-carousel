e87d4e1ea53c63198343cd3e3c67b784096ca1dc
bonvi83 <bonvi83@gmail.com>
2024-01-15 18:03:41 +0100

    di nuovo caos con js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const slides = [`img/01.webp`, `img/02.webp`,`img/03.webp`,`img/04.webp`,`img/05.webp`];
 
 let slidesHtml = ``;
-for (let i=0; i < slides.length; i++) [
+for (let i=0; i < slides.length; i++) {
+
     const slide = slides [i];
 
-    let activeClass;
-    if(i == 0) [
-        activeClass = `active`
-    ]
+    let activeClass = "";
+    if(i == 0) {
+        activeClass = "active"
+    }
 
-    slidesHtml += `<img src="./img/${slide}" class="slide" alt="slide ${i}"s/>`
-]
+    slidesHtml += `<img src="./${slide}" class="slide ${activeClass}" alt="slide ${i}"s/>`
+}
 
 console.log(slidesHtml)
