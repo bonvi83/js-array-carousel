4bd9e6e3a598fe8f30c76858dcf3ef186af5211b
bonvi83 <bonvi83@gmail.com>
2024-01-16 15:06:33 +0100

    add const

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const slides = [`img/01.webp`, `img/02.webp`,`img/03.webp`,`img/04.webp`,`img/05.webp`];
 
 let slidesHtml = ``;
+
+
+const sliderImages = document.querySelector('.immagini');
+const btnUp = document.querySelector('.arrow-up');
+const btnDown = document.querySelector('.arrow-down');
+
+
+
+
+
 for (let i=0; i < slides.length; i++) {
 
     const slide = slides [i];
@@ -11,7 +21,7 @@ for (let i=0; i < slides.length; i++) {
         }
         
         console.log(i, activeClass)
-    slidesHtml += `<img src="./${slide}" class="slide ${activeClass}" alt="slide ${i}"s/>`
+    slidesHtml += `<img src="./${slide}" class="slide ${activeClass}" alt="slide ${i}"s/>`;
 }
 
 console.log(slidesHtml)
